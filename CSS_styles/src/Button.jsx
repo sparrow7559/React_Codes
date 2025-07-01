0253953c67552171794d3c1cb847c4536785c267
sparrow7559 <virajgupta2604@gmail.com>
2025-07-01 15:54:37 +0530

    CSS Styles

--- CSS_styles/src/Button.jsx
+++ CSS_styles/src/Button.jsx
@@ -1,5 +1,15 @@
+// import styles from "./Button.module.css";
+
 function Button() {
-  return <button className="button">Click Me</button>;
+  const styles = {
+    backgroundColor: "hsl(200, 100%, 50%)",
+    color: "white",
+    padding: "10px 20px",
+    borderRadius: "5px",
+    border: "none",
+    cursor: "pointer",
+  };
+  return <button style={styles}>Click Me</button>;
 }
 
 export default Button;
